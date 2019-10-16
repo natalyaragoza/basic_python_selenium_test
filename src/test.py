78b3cbae7fc6689def18aa28266b95e47ed76830
Natalya Ragoza <nragoza@adfone.com>
2019-10-16 10:49:32 +0300

    Added incorrect verification

--- src/test.py
+++ src/test.py
@@ -9,5 +9,6 @@ search_button = driver.find_element_by_xpath("//form[@id = 'search-form']/fields
 search_field.send_keys("Test Automation")
 search_button.click()
 assert "Test automation" in driver.title
+assert "Test 1 automation" in driver.title
 
 driver.quit()
